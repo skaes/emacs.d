0ea5ca1be85cbca039e94d4de501e7c9761ebae0
Stefan Kaes <stefan.kaes@xing.com>
2016-12-02 07:38:07 +0100

    updated rdoc mode

--- site-lisp/rdoc/rdoc-mode.el
+++ site-lisp/rdoc/rdoc-mode.el
@@ -1,18 +1,29 @@
+;;; rdoc.el --- major mode for editing rdoc files
+
+;;; Commentary:
+;;; downloaded from https://raw.githubusercontent.com/jbpros/emacs-setup/master/rdoc/rdoc-mode.el
+
+;;; Code:
+
 (defface rdoc-header-1
-  '((t (:inherit variable-pitch :background "cyan" :weight bold :height 1.4)))
-  "rdoc mode header level 1")
+  '((t (:inherit variable-pitch :foreground "cyan" :weight bold :height 1.4)))
+  "rdoc mode header level 1"
+  :group 'rdoc)
 
 (defface rdoc-header-2
   '((t (:inherit variable-pitch :foreground "cyan" :weight bold :height 1.3)))
-  "rdoc mode header level 2")
+  "rdoc mode header level 2"
+  :group 'rdoc)
 
 (defface rdoc-header-3
   '((t (:inherit variable-pitch :foreground "cyan" :weight bold :height 1.2)))
-  "rdoc mode header level 3")
+  "rdoc mode header level 3"
+  :group 'rdoc)
 
 (defface rdoc-header-4
-  '((t (:inherit variable-pitch :foreground "cyan" :weight bold)))
-  "rdoc mode header level 4")
+  '((t (:inherit variable-pitch :foreground "cyan" :weight bold :height 1.1)))
+  "rdoc mode header level 4"
+  :group 'rdoc)
 
 (define-generic-mode 'rdoc-mode
   ()					;comment-list
@@ -32,5 +43,6 @@
 
 (provide 'rdoc-mode)
 
+;;; rdoc-mode.el ends here
 
 
